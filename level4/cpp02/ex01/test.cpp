88ed33c0b759fb70ee763a7ca33626c692ef6bce
haitooo <haru869pc@gmail.com>
2025-10-30 17:46:53 +0900

    2025_10_30

--- level4/cpp02/ex01/test.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int	main()
-{
-	for (float i = 0; i < 10; i += 0.1f)
-		std::cout << i << std::endl;
-	return (0);
-}
---
+++ level4/cpp02/ex02/Fixed.hpp
@@ -0,0 +1,43 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+# include <cmath>
+
+class Fixed
+{
+	private:
+		int					value;
+		static const int	fractionalBits = 8;
+
+	public:
+		Fixed();
+		Fixed(const int i);
+		Fixed(const float f);
+		Fixed(const Fixed& other);
+		Fixed& operator=(const Fixed& other);
+		Fixed& operator>(const Fixed& other);
+		Fixed& operator<(const Fixed& other);
+		Fixed& operator>=(const Fixed& other);
+		Fixed& operator<=(const Fixed& other);
+		Fixed& operator==(const Fixed& other);
+		Fixed& operator!=(const Fixed& other);
+		Fixed& operator+(const Fixed& other);
+		Fixed& operator-=(const Fixed& other);
+		Fixed& operator*=(const Fixed& other);
+		Fixed& operator/=(const Fixed& other);
+		Fixed& operator++(int);
+		Fixed& operator++();
+		Fixed& operator--(int);
+		Fixed& operator--();
+		~Fixed();
+
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+		float	toFloat( void ) const;
+		int		toInt( void ) const;
+};
+
+std::ostream& operator<<(std::ostream& os, const Fixed& rhs);
+
+#endif
